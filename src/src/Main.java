4532b4823bb7b58eeebadeafa4c495c8072f71ee
samuelrtiz <sjh110125@gmail.com>
2024-01-23 14:50:17 -0400

    1

---
+++ src/src/Main.java
@@ -0,0 +1,56 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+      //punto1
+      String Tipo = "Computadora";
+
+      //punto3
+        boolean disponible = false;
+
+
+      //punto5
+        int numero1 = 4;
+        int numero2 = 9;
+        int resultado = numero1 * numero2;
+
+
+
+        //punto6
+
+        double monto = 775.49;
+        double porcentajeITBIS = 0.18;
+        double montoConITBIS = monto * (1 + porcentajeITBIS);
+
+
+        System.out.println( Tipo );
+        System.out.println("La tasa de cambio del dólar es: " + TASA_DOLAR);
+        System.out.println(MENSAJE_BIENVENIDA);
+        System.out.println("El valor de disponible es: " + disponible);
+        System.out.println("El resultado es: " + resultado);
+        System.out.println("El monto con ITBIS es: " + montoConITBIS);
+    }
+
+
+    //punto2
+    public static final double TASA_DOLAR = 59.50;
+
+
+    //punto4
+    public static final String MENSAJE_BIENVENIDA = "bienvenido a este modulo";
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
